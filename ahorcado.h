ae32a4aed7da825f88ef9d635ea31f1cd3cdd794
kinirochan <43797395+kinirochan@users.noreply.github.com>
2018-10-05 18:36:31 -0300

    Add files via upload

--- ahorcado.h
+++ ahorcado.h
@@ -8,26 +8,68 @@
 using namespace std;
 
 class Ahorcado{
+
 	private:
+
 		char * palabra;
+		char letras_ingresadas[27];
+		bool * letras_acertadas;
 		unsigned intentos;
 		unsigned errores_cometidos;
-		bool * letras_acertadas;
 		unsigned cantidad_letras_ingresadas;
-		char letras_ingresadas[26];
+
+
 	public:
+		/* pre: recibe un string palabra y la cantidad de intentos que tiene el jugador*/
+		/*post:  carga los datos de la palabra y los intentos*/
 		Ahorcado(string palabra, unsigned int intentos);
+
+		/* pre: - */
+		/*post: devuelve los intentos que tiene el jugador */
 		unsigned int obtener_intentos();
+
+		/* pre: -*/
+		/*post: devuelve cuantos errores lleva el jugador hasta el momento*/
 		unsigned obtener_errores_cometidos();
+
+		/* pre: -*/
+		/*post: devuelve la palabra a ser adivinada */
 		char* obtener_palabra();
+
+		/* pre: -*/
+		/*post: devuelve un vector de bool que tiene true en la posicion de la letra si fue adivinada y false en caso contrario*/
 		bool* obtener_letras_acertadas();
+
+		/* pre: -*/
+		/*post: compara si la palabra a adivinar y la arriesgada coinciden viendo asi si el jugador gano o perdio */
 		void comparar_palabra(string palabra_arriesgada);
+
+		/* pre: recibe la letra ingresada por el usuario */
+		/*post: verifica si la letra ingresada esta en la palabra a adivinar y devuelve cuantas veces esta en la misma */
 		int chequear_letra(char letra);
+
+		/* pre: -*/
+		/*post: aumenta los errores cometidos por el jugador */
 		void aumentar_errores();
+
+		/* pre: -*/
+		/*post: devuelve la cantidad de letras que tiene la palabra a adivinar */
 		int contar_letras();
+
+		/* pre: -*/
+		/*post: verifica si el jugador gano o no el juego devolviendo true y false respectivamente */
 		bool gano();
+
+		/* pre: recibe una letra ingresada por el usuario */
+		/*post: devuelve true si la letra ingresada ya habia sido ingresada anteriormente  */
 		bool letra_es_repetida(char letra);
+
+		/* pre: -*/
+		/*post: comprueba si la partida finalizo o no devolviendo true y false respectivamente*/
 		bool comprobar_partida();
+
+		/* pre: -*/
+		/*post: libera la memoria que fue pedida a lo largo del programa */
 		~Ahorcado();
 };
 
--- disenio.h
+++ disenio.h
@@ -5,8 +5,16 @@
 
 using namespace std;
 
+/* pre: recibe los intentos maximos y los errores cometidos ambos datos deben ser validos */
+/*post:  imprime por pantalla una persona con tantas partes del cuerpo como errores haya cometido el jugador*/
 void dibujar_persona(int errores, int intentos_max);
+
+/* pre: recibe la palabra a adivinar un vector bool con cuantas letras fueron adivinadas y la cantidad de letras, todos datos validos*/
+/*post:  dibuja las letras que fueron adivinadas por pantalla*/
 void dibujar_letras(char* palabra, bool adivinadas[], int cantidad_letras);
+
+/* pre: recibe la palabra a adivinar y la cantidad de letras de la misma */
+/*post: imprime por pantalla tantos guiones como letras queden por adivinar*/
 void dibujar_letras(char* palabra, int cantidad_letras);
 
 #endif
